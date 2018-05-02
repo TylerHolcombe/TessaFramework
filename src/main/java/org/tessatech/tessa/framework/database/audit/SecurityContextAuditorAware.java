111a1bd0b723bfa131627244974193eb12571d80
Mike <mwerzen@gmail.com>
2018-05-02 19:59:00 -0400

    Added Auditable Entities to Framework

---
+++ src/main/java/org/tessatech/tessa/framework/database/audit/SecurityContextAuditorAware.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2015-2018 TessaTech LLC.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file
+ *  except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package org.tessatech.tessa.framework.database.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import org.tessatech.tessa.framework.core.security.context.SecurityContext;
+import org.tessatech.tessa.framework.core.security.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class SecurityContextAuditorAware implements AuditorAware<String>
+{
+
+	@Override
+	public Optional<String> getCurrentAuditor()
+	{
+		String auditor = null;
+
+		if (SecurityContextHolder.getContextOptional().isPresent())
+		{
+			auditor = SecurityContextHolder.getContext().getUserId();
+		}
+
+		return Optional.of(auditor);
+	}
+}
---
+++ src/main/java/org/tessatech/tessa/framework/database/entity/Auditable.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2015-2018 TessaTech LLC.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file
+ *  except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package org.tessatech.tessa.framework.database.entity;
+
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable
+{
+	@CreatedBy
+	protected String createdBy;
+
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date createdDate;
+
+	@LastModifiedBy
+	protected String lastModifiedBy;
+
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	@Version
+	protected Date lastModifiedDate;
+
+	public String getCreatedBy()
+	{
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy)
+	{
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedDate()
+	{
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate)
+	{
+		this.createdDate = createdDate;
+	}
+
+	public String getLastModifiedBy()
+	{
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy)
+	{
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	public Date getLastModifiedDate()
+	{
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate)
+	{
+		this.lastModifiedDate = lastModifiedDate;
+	}
+}
